2912576db4b7997983857323abf1e86928137ce8
PaoloGiraudi <pgiraudi94@gmail.com>
2023-04-26 08:47:22 +0200

    bump dependencies

--- src/components/beat-toggle.svelte
+++ src/components/beat-toggle.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   export let handleClick: () => void;
   export let beat: boolean;
-  export let highlighted: Boolean;
+  export let highlighted: boolean;
 </script>
 
 <div
-  class={`border-2 border-accent ${beat ? "bg-accent" : "bg-transparent"} ${
+  class={`border-2 border-accent ${beat ? "bg-accent" : ""} ${
     highlighted ? "bg-green-500" : ""
   }`}
   on:click={handleClick}
