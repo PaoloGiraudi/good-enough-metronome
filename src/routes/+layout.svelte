c7509a7c63b6c72603f273a57f7a3daa06b69a88
PaoloGiraudi <pgiraudi94@gmail.com>
2023-05-03 16:57:19 +0200

    add PWA

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,24 +12,24 @@
 </script>
 
 <svelte:head>
+	<meta name="theme-color" media="(prefers-color-scheme: light)" content="#fffcfa" />
+	<meta name="theme-color" media="(prefers-color-scheme: dark)" content="#0A0908" />
 	<title>GEM: Good enough metronome</title>
 </svelte:head>
 
 <main
-	class="container mx-auto flex flex-col p-4 justify-between sm:w-1/2 sm:max-w-lg sm:h-2/3 sm:shadow-2xl sm:shadow-neutral"
+	class="container mx-auto flex flex-col p-4 justify-between h-full sm:w-1/2 sm:max-w-lg sm:h-2/3 sm:shadow-2xl sm:shadow-neutral"
 >
-	<div>
-		<slot />
-		<button
-			class="btn btn-circle btn-neutral absolute bottom-4 right-4 sm:top-8 sm:right-8"
-			on:click={() => theme.set($theme === 'light' ? 'dark' : 'light')}
-		>
-			{#if $theme === 'dark'}
-				<Sun />
-			{/if}
-			{#if $theme === 'light'}
-				<Moon />
-			{/if}
-		</button>
-	</div>
+	<slot />
+	<button
+		class="btn btn-circle btn-neutral absolute bottom-4 right-4 sm:top-8 sm:right-8"
+		on:click={() => theme.set($theme === 'light' ? 'dark' : 'light')}
+	>
+		{#if $theme === 'dark'}
+			<Sun />
+		{/if}
+		{#if $theme === 'light'}
+			<Moon />
+		{/if}
+	</button>
 </main>
