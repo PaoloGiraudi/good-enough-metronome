e45d479a143cbbab48d8289364d4f35c9c8163fc
PaoloGiraudi <lp.web@pm.me>
2026-08-31 20:53:50 +0200

    bump

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -5,55 +5,55 @@
 
 const sw = self as unknown as ServiceWorkerGlobalScope;
 
-import { build, files, version } from '$service-worker';
+import { build, files, version } from "$service-worker";
 
 const CACHE = `cache-${version}`;
 
 const ASSETS = [...build, ...files];
 
-sw.addEventListener('install', (event) => {
-	async function addFilesToCache() {
-		const cache = await caches.open(CACHE);
-		await cache.addAll(ASSETS);
-	}
+sw.addEventListener("install", (event) => {
+  async function addFilesToCache() {
+    const cache = await caches.open(CACHE);
+    await cache.addAll(ASSETS);
+  }
 
-	event.waitUntil(addFilesToCache());
-	sw.skipWaiting();
+  event.waitUntil(addFilesToCache());
+  sw.skipWaiting();
 });
 
-sw.addEventListener('activate', (event) => {
-	async function deleteOldCaches() {
-		for (const key of await caches.keys()) {
-			if (key !== CACHE) await caches.delete(key);
-		}
-	}
+sw.addEventListener("activate", (event) => {
+  async function deleteOldCaches() {
+    for (const key of await caches.keys()) {
+      if (key !== CACHE) await caches.delete(key);
+    }
+  }
 
-	event.waitUntil(deleteOldCaches());
+  event.waitUntil(deleteOldCaches());
 });
 
-sw.addEventListener('fetch', (event) => {
-	if (event.request.method !== 'GET') return;
+sw.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
 
-	async function respond() {
-		const url = new URL(event.request.url);
-		const cache = await caches.open(CACHE);
+  async function respond() {
+    const url = new URL(event.request.url);
+    const cache = await caches.open(CACHE);
 
-		if (ASSETS.includes(url.pathname)) {
-			return cache.match(event.request);
-		}
+    if (ASSETS.includes(url.pathname)) {
+      return cache.match(event.request);
+    }
 
-		try {
-			const response = await fetch(event.request);
+    try {
+      const response = await fetch(event.request);
 
-			if (response.status === 200) {
-				cache.put(event.request, response.clone());
-			}
+      if (response.status === 200) {
+        cache.put(event.request, response.clone());
+      }
 
-			return response;
-		} catch {
-			return cache.match(event.request);
-		}
-	}
+      return response;
+    } catch {
+      return cache.match(event.request);
+    }
+  }
 
-	event.respondWith(respond() as Response | PromiseLike<Response>);
+  event.respondWith(respond() as Response | PromiseLike<Response>);
 });
